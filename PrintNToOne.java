653c926b279b37547be564d789174c0e516f58a2
snehasishguharoy <snehasishguharoy871>
2022-05-01 23:37:34 +0530

    One file is committed

--- PrintNToOne.java
+++ PrintNToOne.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class PrintNToOne {
 
     public static void main(String[] args) {
-        System.out.println("Recursion");
+        Scanner sc=new Scanner(System.in);
+
+        printNToOne(sc.nextInt());
+
+    }
+
+    public static void printNToOne(int n){
+        if(n == 0){
+            return;
+        }
+        System.out.println(n);
+        printNToOne(n-1);
+
     }
 }
